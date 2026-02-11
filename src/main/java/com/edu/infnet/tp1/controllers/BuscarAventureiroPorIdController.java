5932aa4e33607adea50a1d292c506192950cc38e
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-11 18:03:26 -0300

    fix: adjust error response handling in BuscarAventureiroPorIdController

--- src/main/java/com/edu/infnet/tp1/controllers/BuscarAventureiroPorIdController.java
+++ src/main/java/com/edu/infnet/tp1/controllers/BuscarAventureiroPorIdController.java
@@ -1,5 +1,7 @@
 package com.edu.infnet.tp1.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.UUID;
 
 import com.edu.infnet.tp1.models.Aventureiro;
 import com.edu.infnet.tp1.services.BuscarAventureiroPorIdService;
@@ -30,11 +30,10 @@ public class BuscarAventureiroPorIdController {
       return ResponseEntity.status(HttpStatus.OK).body(aventureiro);
     } catch (ResponseStatusException e) {
       ErrorMessage error = new ErrorMessage(
-          HttpStatus.NOT_FOUND.value(),
           "Solicitação inválida",
           e.getMessage());
 
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+      return ResponseEntity.status(e.getStatusCode()).body(error);
     }
   }
 }
