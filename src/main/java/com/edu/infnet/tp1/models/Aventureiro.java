7315586e0ebae054fb86f4a5aec577e36740029f
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-11 09:27:13 -0300

    fix: correct import statement and format Aventureiro class

--- src/main/java/com/edu/infnet/tp1/models/Aventureiro.java
+++ src/main/java/com/edu/infnet/tp1/models/Aventureiro.java
@@ -3,7 +3,7 @@ package com.edu.infnet.tp1.models;
 import java.util.Optional;
 import java.util.UUID;
 
-import com.edu.infnet.tp1.interfaces.Classes;
+import com.edu.infnet.tp1.enums.Classes;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,14 +14,14 @@ import lombok.Setter;
  * @author nathan.vieira
  */
 
-@Getter @Setter
+@Getter
+@Setter
 @AllArgsConstructor
-
 public class Aventureiro {
   private UUID id;
   private String nome;
   private Classes classe;
   private Number nivel;
   private Boolean ativo;
-  private Optional<String>  companheiro; 
+  private Optional<String> companheiro;
 }
