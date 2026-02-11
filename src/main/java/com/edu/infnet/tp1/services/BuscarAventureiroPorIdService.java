3fa15ee0ea1689f4713f617f82e3e2b3bbe45faf
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-11 09:25:34 -0300

    feat: update BuscarAventureiroPorIdService to retrieve Aventureiro by ID

--- src/main/java/com/edu/infnet/tp1/services/BuscarAventureiroPorIdService.java
+++ src/main/java/com/edu/infnet/tp1/services/BuscarAventureiroPorIdService.java
@@ -1,6 +1,22 @@
 package com.edu.infnet.tp1.services;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.edu.infnet.tp1.data.AventureiroData;
+import com.edu.infnet.tp1.models.Aventureiro;
+
+@Service
 public class BuscarAventureiroPorIdService {
-public void exec() {
+  @Autowired
+  AventureiroData aventureiroData;
+
+  public Aventureiro exec(UUID id) {
+    return aventureiroData.buscarAventureiroPorId(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
   }
 }
