07715aaa93bb2b1a6ac1c3e19e1336113c1d3253
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-11 09:24:30 -0300

    feat: replace Especies interface with enum

---
+++ src/main/java/com/edu/infnet/tp1/enums/Especies.java
@@ -0,0 +1,8 @@
+package com.edu.infnet.tp1.enums;
+
+public enum Especies {
+  LOBO,
+  CORUJA,
+  GOLEM,
+  DRAGAO_MINIATURA,
+}
--- src/main/java/com/edu/infnet/tp1/interfaces/Especies.java
+++
@@ -1,8 +0,0 @@
-package com.edu.infnet.tp1.interfaces;
-
-public interface Especies {
-String  LOBO= "LOBO";
-String CORUJA= "CORUJA";
-String GOLEM= "GOLEM";
-String DRAGAO_MINIATURA= "DRAGAO_MINIATURA";
-}
