deeb3bfa8e7f99b958705ba7daddd9be7e04421c
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-11 09:25:21 -0300

    feat: update ListarAventureirosService to handle pagination and validation

--- src/main/java/com/edu/infnet/tp1/services/ListarAventureirosService.java
+++ src/main/java/com/edu/infnet/tp1/services/ListarAventureirosService.java
@@ -1,10 +1,33 @@
 package com.edu.infnet.tp1.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.edu.infnet.tp1.data.AventureiroData;
+import com.edu.infnet.tp1.models.Aventureiro;
+import com.edu.infnet.tp1.shared.dtos.PaginationQueryDto;
 
 @Service
 public class ListarAventureirosService {
-  public void exec() {
+  @Autowired
+  AventureiroData aventureiroData;
+
+  public List<Aventureiro> exec(PaginationQueryDto params) {
+    if (params.page() < 0) {
+      throw new IllegalArgumentException("A página não pode ser menor que 0");
+    }
+
+    if (params.size() < 1 || params.size() > 50)
+      throw new IllegalArgumentException("O size deve estar entre 1 e 50");
+
+    return aventureiroData.listarAventureiros(params);
+
+  }
+
+  public int contarAventureiros(PaginationQueryDto params) {
+    return aventureiroData.contarAventureiros(params);
   }
 
 }
