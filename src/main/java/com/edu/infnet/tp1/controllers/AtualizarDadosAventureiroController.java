1cfe801e1bef7f41b5612752613cef11e51053c9
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-11 18:02:56 -0300

    feat: implement PATCH endpoint for updating Aventureiro details

--- src/main/java/com/edu/infnet/tp1/controllers/AtualizarDadosAventureiroController.java
+++ src/main/java/com/edu/infnet/tp1/controllers/AtualizarDadosAventureiroController.java
@@ -1,11 +1,20 @@
 package com.edu.infnet.tp1.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.edu.infnet.tp1.models.Aventureiro;
 import com.edu.infnet.tp1.services.AtualizarDadosAventureiroService;
+import com.edu.infnet.tp1.shared.dtos.AtualizarAventureiroRequestDto;
+import com.edu.infnet.tp1.shared.errors.ErrorMessage;
 
 @RestController
 @RequestMapping("/api/aventureiros")
@@ -13,7 +22,20 @@ public class AtualizarDadosAventureiroController {
   @Autowired
   AtualizarDadosAventureiroService atualizarDadosAventureiroService;
 
-  @PostMapping("/{id}")
-  public void atualizarDadosAventureiro() {
+  @PatchMapping("/{id}")
+  public ResponseEntity<?> atualizarDadosAventureiro(
+      @PathVariable UUID id,
+      @RequestBody AtualizarAventureiroRequestDto aventureiroAtualizado) {
+    try {
+      Aventureiro resultado = atualizarDadosAventureiroService.exec(id, aventureiroAtualizado);
+
+      return ResponseEntity.ok().body(resultado);
+    } catch (ResponseStatusException e) {
+      ErrorMessage error = new ErrorMessage(
+          "Solicitação inválida",
+          e.getMessage());
+
+      return ResponseEntity.status(e.getStatusCode()).body(error);
+    }
   }
 }
--- src/main/java/com/edu/infnet/tp1/services/AtualizarDadosAventureiroService.java
+++ src/main/java/com/edu/infnet/tp1/services/AtualizarDadosAventureiroService.java
@@ -1,9 +1,23 @@
 package com.edu.infnet.tp1.services;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.edu.infnet.tp1.data.AventureiroData;
+import com.edu.infnet.tp1.models.Aventureiro;
+import com.edu.infnet.tp1.shared.dtos.AtualizarAventureiroRequestDto;
 
 @Service
 public class AtualizarDadosAventureiroService {
-  public void exec() {
+  @Autowired
+  AventureiroData aventureiroData;
+
+  public Aventureiro exec(UUID id, AtualizarAventureiroRequestDto aventureiroAtualizado) {
+    return aventureiroData.atualizarAventureiro(id, aventureiroAtualizado)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Aventureiro não encontrado!"));
   }
 }
