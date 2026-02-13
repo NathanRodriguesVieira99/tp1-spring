a99dd1be185b609ea2dee3f9e49db0bb02d32685
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-13 17:04:05 -0300

    feat: add Companheiro field to PaginationResponseDto for enhanced response structure

--- src/main/java/com/edu/infnet/tp1/shared/dtos/PaginationResponseDto.java
+++ src/main/java/com/edu/infnet/tp1/shared/dtos/PaginationResponseDto.java
@@ -3,6 +3,7 @@ package com.edu.infnet.tp1.shared.dtos;
 import java.util.UUID;
 
 import com.edu.infnet.tp1.enums.Classes;
+import com.edu.infnet.tp1.models.Companheiro;
 
 // DTO -> Para resposta das rotas GET com filtros e pagination (retorna um aventureiro basicamente)
 public record PaginationResponseDto(
@@ -10,5 +11,7 @@ public record PaginationResponseDto(
     String nome,
     Classes classe,
     Number nivel,
-    Boolean ativo) {
+    Boolean ativo,
+    Companheiro companheiro
+  ) {
 }
