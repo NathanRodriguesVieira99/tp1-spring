b75369c73a6fc5eaa42b78605de127f8a5cab7b1
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-13 10:26:42 -0300

    refactor: replace ResponseStatusException with custom AventureiroNotFoundException in AtualizarDadosAventureiroService

--- src/main/java/com/edu/infnet/tp1/services/AtualizarDadosAventureiroService.java
+++ src/main/java/com/edu/infnet/tp1/services/AtualizarDadosAventureiroService.java
@@ -3,13 +3,12 @@ package com.edu.infnet.tp1.services;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.edu.infnet.tp1.data.AventureiroData;
 import com.edu.infnet.tp1.models.Aventureiro;
 import com.edu.infnet.tp1.shared.dtos.AtualizarAventureiroRequestDto;
+import com.edu.infnet.tp1.shared.exceptions.AventureiroNotFoundException;
 
 @Service
 public class AtualizarDadosAventureiroService {
@@ -18,6 +17,6 @@ public class AtualizarDadosAventureiroService {
 
   public Aventureiro exec(UUID id, AtualizarAventureiroRequestDto aventureiroAtualizado) {
     return aventureiroData.atualizarAventureiro(id, aventureiroAtualizado)
-        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Aventureiro não encontrado!"));
+        .orElseThrow(() -> new AventureiroNotFoundException());
   }
 }
