1c338c3b5c1fd244bb5195fd830c9a51a1298a15
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-13 10:27:55 -0300

    feat: add AventureiroInvalidParamsException for improved parameter validation

---
+++ src/main/java/com/edu/infnet/tp1/shared/exceptions/AventureiroInvalidParamsException.java
@@ -0,0 +1,14 @@
+package com.edu.infnet.tp1.shared.exceptions;
+
+// Exception customizada extendendo a padrão do Java
+public class AventureiroInvalidParamsException extends RuntimeException {
+
+  public AventureiroInvalidParamsException() {
+    super("Parametros inválidos");
+  }
+
+  // Instancia a mensagem customizada
+  public AventureiroInvalidParamsException(String message) {
+    super(message);
+  }
+}
