835564c7fc85d34769061bcfa7230ada8cec4cf8
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-13 10:30:36 -0300

    refactor: update ErrorMessage class to standardize custom exception responses

--- src/main/java/com/edu/infnet/tp1/shared/errors/ErrorMessage.java
+++ src/main/java/com/edu/infnet/tp1/shared/errors/ErrorMessage.java
@@ -1,13 +1,18 @@
 package com.edu.infnet.tp1.shared.errors;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Classe responsável por padronizar o retorno das exceptions customizadas
+ */
+@AllArgsConstructor
 @Getter
 @Setter
-@AllArgsConstructor
 public class ErrorMessage {
-  private String mensagem;
-  private String detalhes;
+  private HttpStatus status;
+  private String message;
 }
