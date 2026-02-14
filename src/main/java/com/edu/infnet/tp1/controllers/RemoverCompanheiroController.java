72d3b7b715a599bbffb62a6038abca17039e7fcf
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-14 15:56:26 -0300

    refactor: update RemoverCompanheiroController to use constructor injection and implement service

--- src/main/java/com/edu/infnet/tp1/controllers/RemoverCompanheiroController.java
+++ src/main/java/com/edu/infnet/tp1/controllers/RemoverCompanheiroController.java
@@ -1,21 +1,31 @@
 package com.edu.infnet.tp1.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.edu.infnet.tp1.models.Aventureiro;
 import com.edu.infnet.tp1.services.RemoverCompanheiroService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/companheiros")
+@RequiredArgsConstructor
 public class RemoverCompanheiroController {
-  @Autowired
-  RemoverCompanheiroService removerCompanheiroService;
 
-  @DeleteMapping("/{id}")
+ private final RemoverCompanheiroService removerCompanheiroService;
 
-  public void removerCompanheiro() {
-  
+  @DeleteMapping("/delete/{id}")
+
+  public ResponseEntity<Aventureiro> removerCompanheiro(@PathVariable UUID id) {
+    Aventureiro aventureiroComCompanheiroEncontrado = removerCompanheiroService.exec(id);
+
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).body(aventureiroComCompanheiroEncontrado);
   }
 }
