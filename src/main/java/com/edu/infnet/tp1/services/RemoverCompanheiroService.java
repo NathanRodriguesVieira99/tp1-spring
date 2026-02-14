abc18555e6fddc3f862640968ff55d974b8b25bb
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-14 15:54:44 -0300

    feat: implement RemoverCompanheiroService to remove companion by ID

--- src/main/java/com/edu/infnet/tp1/services/RemoverCompanheiroService.java
+++ src/main/java/com/edu/infnet/tp1/services/RemoverCompanheiroService.java
@@ -1,10 +1,20 @@
 package com.edu.infnet.tp1.services;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
-@Service
-public class RemoverCompanheiroService {
+import com.edu.infnet.tp1.data.AventureiroData;
+import com.edu.infnet.tp1.models.Aventureiro;
 
-  public void exec() {
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RemoverCompanheiroService {
+  private final AventureiroData aventureiroData;
+
+  public Aventureiro exec(UUID id) {
+    return aventureiroData.removerCompanheiro(id);
   }
 }
