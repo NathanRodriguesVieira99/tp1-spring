3bfccd8b73660089efac0d0698f8abde0b7284df
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-16 09:40:04 -0300

    feat: implement endpoint to remove from guild

--- src/main/java/com/edu/infnet/tp1/controllers/EncerrarVinculoGuildaController.java
+++ src/main/java/com/edu/infnet/tp1/controllers/EncerrarVinculoGuildaController.java
@@ -1,15 +1,30 @@
 package com.edu.infnet.tp1.controllers;
 
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.edu.infnet.tp1.models.Aventureiro;
+import com.edu.infnet.tp1.services.EncerrarVinculoGuildaService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
-@RequestMapping("/api/aventureiro")
+@RequestMapping("/api/aventureiros/guilda")
+@RequiredArgsConstructor
 public class EncerrarVinculoGuildaController {
 
-  @PatchMapping("/{id}")
-  public void encerrarVinculoGuilda() {
+  private final EncerrarVinculoGuildaService encerrarVinculoGuildaService;
 
+  @PatchMapping("/remove/{id}")
+  public ResponseEntity<?> encerrarVinculoGuilda(@PathVariable UUID id) {
+    Aventureiro aventureiroAtivo = encerrarVinculoGuildaService.exec(id);
+
+    return ResponseEntity.status(HttpStatus.OK).body(aventureiroAtivo);
   }
 }
--- src/main/java/com/edu/infnet/tp1/services/EncerrarVinculoGuildaService.java
+++ src/main/java/com/edu/infnet/tp1/services/EncerrarVinculoGuildaService.java
@@ -1,13 +1,21 @@
 package com.edu.infnet.tp1.services;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
+
+import com.edu.infnet.tp1.data.AventureiroData;
+import com.edu.infnet.tp1.models.Aventureiro;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class EncerrarVinculoGuildaService {
-  public void exec() {
 
+  private final AventureiroData aventureiroData;
+
+  public Aventureiro exec(UUID id) {
+    return aventureiroData.encerrarVinculoGuilda(id);
   }
 }
