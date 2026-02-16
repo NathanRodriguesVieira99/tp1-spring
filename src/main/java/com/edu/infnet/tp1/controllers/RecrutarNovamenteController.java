8b5bda951c219573cac7aefe4e057b7f50892aa2
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-02-16 09:39:48 -0300

    feat: implement recruit endpoint

--- src/main/java/com/edu/infnet/tp1/controllers/RecrutarNovamenteController.java
+++ src/main/java/com/edu/infnet/tp1/controllers/RecrutarNovamenteController.java
@@ -1,16 +1,30 @@
 package com.edu.infnet.tp1.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.edu.infnet.tp1.models.Aventureiro;
 import com.edu.infnet.tp1.services.RecrutarNovamenteService;
 
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/aventureiros/guilda")
+@RequiredArgsConstructor
 public class RecrutarNovamenteController {
-  @Autowired
-  RecrutarNovamenteService recrutarNovamenteService;
 
-  @PatchMapping("/{id}")
-  public void recrutarNovamente() {
+  private final RecrutarNovamenteService recrutarNovamenteService;
+
+  @PatchMapping("/recruit/{id}")
+  public ResponseEntity<?> encerrarVinculoGuilda(@PathVariable UUID id) {
+    Aventureiro aventureiroInativo = recrutarNovamenteService.exec(id);
+
+    return ResponseEntity.status(HttpStatus.OK).body(aventureiroInativo);
   }
-
 }
--- src/main/java/com/edu/infnet/tp1/services/RecrutarNovamenteService.java
+++ src/main/java/com/edu/infnet/tp1/services/RecrutarNovamenteService.java
@@ -1,12 +1,20 @@
 package com.edu.infnet.tp1.services;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
+
+import com.edu.infnet.tp1.data.AventureiroData;
+import com.edu.infnet.tp1.models.Aventureiro;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class RecrutarNovamenteService {
-  public void exec() {
+  private final AventureiroData aventureiroData;
+
+  public Aventureiro exec(UUID id) {
+    return aventureiroData.recrutarNovamente(id);
   }
 }
