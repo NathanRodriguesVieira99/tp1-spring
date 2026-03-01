310f17ad453df3c8d7e1597615b235287a18e34b
Nathan Rodrigues <nathanrodriguesvieira99@gmail.com>
2026-03-01 17:13:35 -0300

    feat: add enums for NivelPerigo, PapelMissao, and StatusMissao

---
+++ src/main/java/com/edu/infnet/tp1/enums/NivelPerigo.java
@@ -0,0 +1,8 @@
+package com.edu.infnet.tp1.enums;
+
+public enum NivelPerigo {
+  BAIXO,
+  MEDIO,
+  ALTO,
+  EXTREMO
+}
---
+++ src/main/java/com/edu/infnet/tp1/enums/PapelMissao.java
@@ -0,0 +1,8 @@
+package com.edu.infnet.tp1.enums;
+
+public enum PapelMissao {
+  LIDER,
+  SUPORTE,
+  DPS,
+  CURADOR
+}
