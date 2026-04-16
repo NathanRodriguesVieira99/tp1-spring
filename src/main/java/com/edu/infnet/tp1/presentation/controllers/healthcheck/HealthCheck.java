8d875a9a646a364ed195299c570ba9ddace96c25
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-16 09:00:19 -0300

    refactor(controllers): move HealthCheck para pasta correta

--- src/main/java/com/edu/infnet/tp1/presentation/controllers/healthcheck/HealthCheck.java
+++ src/main/java/com/edu/infnet/tp1/presentation/controllers/healthcheck/HealthCheck.java
@@ -1,4 +1,4 @@
-package com.edu.infnet.tp1.presentation.controllers;
+package com.edu.infnet.tp1.presentation.controllers.healthcheck;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
